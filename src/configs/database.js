b9b1bb517388f4afd40bfaf7805bb1fab21a58f1
1337_DaKL <luongtrinh2k3ndad@gmail.com>
2024-10-08 10:22:02 +0700

    check

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 module.exports.connect = async () => {
     try{
-        await mongoose.connect("mongodb://localhost:27017/web");
+        await mongoose.connect("mongodb+srv://luongtrinh2k3ndad:cUAJ1z49CLXJ8AVy@cluster0.tsdoq.mongodb.net/web");
         console.log("Connect database sucess!");
     }catch(error)
     {
