436007611ed223852da28108e2cce7a0ab8ffde2
1337DaKL <luongtrinh2k3ndad@gmail.com>
2024-11-16 23:49:12 +0700

    Kiểm tra dữ liệu đầu vào để tránh lỗi chèn mã sql và dữ liệu rỗng có thể gây sặp server

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,17 +1,42 @@
-const loginService = require("../services/loginService")
-const User = require("../models/user.model.js")
-const login = async (req, res) => {
-    const {username, password} = req.body
-    console.log(username, password)
-    const result = await loginService.login(username, password)
-    if(result == 1) {
-        const user = new User(req.body);
-        await user.save();
-        return res.render('index')
-    }
-    else {
-        return res.render('qldt', {err: "err"})
-    }
-}
+const { body, validationResult } = require('express-validator');
+const loginService = require("../services/loginService");
+const User = require("../models/user.model.js");
 
-module.exports = { login }
+const login = [
+    body('username')
+        .isAlphanumeric()
+        .withMessage('Username phải là ký tự chữ và số'),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password phải có ít nhất 6 ký tự'),
+
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            console.log(errors.array()[0])
+            if (errors.array().length == 1) {
+                return res.render('qldt', { error1: errors.array()[0] });
+            }
+
+
+            return res.render('qldt', { error1: errors.array()[0], error2: errors.array()[1] });
+
+
+        }
+        const { username, password } = req.body;
+
+        const isAuthenticated = await loginService.login(username, password);
+
+        if (isAuthenticated) {
+            const user = new User(req.body);
+            await user.save();
+
+            return res.render('index');
+        } else {
+            return res.render('qldt', { err: "Mật khẩu không chính xác" });
+        }
+    }
+];
+
+module.exports = { login };
