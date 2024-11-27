837dcba4bf5caa0606e844ea48852093ef5408c8
Duong Quang Hao <hao510duong@gmail.com>
2024-11-27 23:00:08 +0700

    fix conflict

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -6,59 +6,6 @@ function sleep(ms) {
 }
 
 const login = async (username, password) => {
-<<<<<<< HEAD
-    let browser;
-    try {
-        // Khởi động Puppeteer với các cờ phù hợp
-        browser = await puppeteer.launch({
-            headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage', // Giảm yêu cầu bộ nhớ
-                '--disable-gpu', // Tắt GPU (trên máy không có đồ họa)
-                '--no-first-run',
-                '--no-zygote'
-            ],
-            executablePath: '/usr/bin/chromium-browser'
-        });
-
-        const page = await browser.newPage();
-        page.setDefaultTimeout(200000);
-
-        // Điều hướng tới trang đăng nhập
-        await page.goto('https://qldt.ptit.edu.vn/#/home', { waitUntil: 'networkidle0' });
-
-        await page.waitForSelector('input[name="username"]', { timeout: 5000 });
-        await page.type('input[name="username"]', username);
-        await page.type('input[type="password"]', password);
-
-        const loginButton = await page.$('button[class="btn btn-primary mb-1 ng-star-inserted"]');
-        if (!loginButton) {
-            console.log('Nút đăng nhập không tồn tại!');
-            throw new Error('Nút đăng nhập không tồn tại!');
-        }
-        await loginButton.click();
-        await sleep(2000);
-
-        const alertExists = await page.$$('.alert.alert-danger');
-        if (alertExists.length !== 0) {
-            console.log('Đăng nhập không thành công');
-            return 0; // Lỗi đăng nhập
-        }
-
-        console.log('Đăng nhập thành công');
-        return 1; // Đăng nhập thành công
-    } catch (error) {
-        console.error('Lỗi trong quá trình đăng nhập:', error.message);
-        return 0; // Lỗi hệ thống
-    } finally {
-        if (browser) {
-            await browser.close();
-        }
-    }
-};
-=======
     const browser = await puppeteer.launch({
         headless: true,
     });
@@ -82,5 +29,4 @@ const login = async (username, password) => {
         return 1
     }
 }
-module.exports = { login   }
->>>>>>> production
+module.exports = { login  }
